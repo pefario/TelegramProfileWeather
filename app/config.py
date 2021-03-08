550227cf2591a9ea3bca5069fed539bccc0b3e75
pefario <wl0lxxl0lw@gmail.com>
2021-03-08 21:02:33 +0500

    Create repo

---
+++ app/config.py
@@ -0,0 +1,3 @@
+weather_token = ''  # Токен для работы с API OpenWeather
+telegram_id = 0  # ID для работы с API Telegram
+telegram_hash = ''  # hash для работы с API Telegram
---
+++ app/main.py
@@ -0,0 +1,41 @@
+from pyowm import OWM
+from datetime import datetime
+from telethon import TelegramClient
+from telethon.tl.functions.account import UpdateProfileRequest
+import asyncio
+from config import *
+
+owm = OWM(weather_token)
+mgr = owm.weather_manager()
+
+telegram_client = TelegramClient('TelegramProfileWeather', telegram_id, telegram_hash)
+telegram_client.start()
+
+emojis = {
+    'clouds': '☁️',
+    'clear': '☀️',
+    'snow': '❄️',
+    'rain': '🌧'
+}
+
+
+async def update_description():
+    """Добавляем информацию о погоде в описание пользователя"""
+    while True:
+        observation = mgr.weather_at_place('Ekaterinburg')
+        weather = observation.weather
+        description = f'Сейчас в Екатеринбурге {weather.temperature("celsius")["temp"]} '
+
+        current_time = datetime.now()
+        if current_time.hour <= 6 or current_time.hour >= 22:
+            description += '🌙'
+        else:
+            description += emojis.get(weather.status.lower(), '🌆')
+
+        await telegram_client(UpdateProfileRequest(about=description))
+        await asyncio.sleep(3600)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(update_description())
